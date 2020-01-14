5258b044681f22ed3225f2ed1b22efcf78abcb04
Rolando Sanchez <rsanchezbaltodano@gmail.com>
2020-01-14 15:55:50 -0600

    cambio de puerto

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 const cron = require('node-cron');
 var bodyparse = require('body-parser');
 const transaction = require('./controller/fuel-service-transaction');
